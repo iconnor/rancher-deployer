dc69a7882732b476874862b3ec59f468683d1ed3
Ian Connor <iconnor@projectlounge.com>
2017-06-14 11:34:59 +1000

    Add circle deploy number

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,10 @@ curl -s -L -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" $PROJECT_CONFIG_URL -o c
 unzip config.zip
 rm config.zip
 
+if [ -z "$CIRCLE_BUILD_NUM" ]; then
+  sed "s/:v[0-9][0-9][0-9]/:v$CIRCLE_BUILD_NUM/g" config/docker-compose.yml
+fi
+
 # Do Upgrade
 echo ""
 echo "-> Updating service $RANCHER_SERVICE_NAME on $RANCHER_STACK_NAME"
